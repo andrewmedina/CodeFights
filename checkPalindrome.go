e9d87c0dc06d686a10672169d324d93f76b4c4ff
Andrew Marten Medina <ammy2015@mymail.pomona.edu>
2018-06-25 17:01:34 -0700

    Create checkPalindrome.go

---
+++ checkPalindrome.go
@@ -0,0 +1,8 @@
+func checkPalindrome(inputString string) bool {
+    for i := 0; i < len(inputString) / 2; i++ {
+        if inputString[i] != inputString[len(inputString) - 1 - i] {
+            return false
+        }
+    }
+    return true
+}
