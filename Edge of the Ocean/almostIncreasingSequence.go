4c6909d6c3b36f03100597aa423b07711b64afd8
Andrew Marten Medina <ammy2015@mymail.pomona.edu>
2018-06-26 18:11:49 -0700

    Update almostIncreasingSequence.go

--- Edge of the Ocean/almostIncreasingSequence.go
+++ Edge of the Ocean/almostIncreasingSequence.go
@@ -1,14 +1,14 @@
 func almostIncreasingSequence(sequence []int) bool {
-    screwups, tail := 0, 0
-    for i := 1; i < len(sequence); i++ {
-        if sequence[tail] < sequence[i] {
-            tail = i;
-        } else {
-            if tail == 0 || sequence[i] > sequence[tail - 1] {
-				tail = i;
+	screwups, tail := 0, 0
+	for i := 1; i < len(sequence); i++ {
+		if sequence[tail] < sequence[i] {
+			tail = i
+		} else {
+			if tail == 0 || sequence[i] > sequence[tail - 1] {
+				tail = i
 			}
-			screwups++;
+			screwups++
 		}
 	}
-	return screwups == 1;
+	return screwups == 1
 }
